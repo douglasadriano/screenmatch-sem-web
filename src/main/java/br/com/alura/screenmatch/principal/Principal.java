beaa0c4b0978aaab5d14e0ed504e555fe15b4a1e
douglas <da-zf@hotmail.com>
2023-10-06 17:13:50 -0300

    Acrescentando dados dos episódios.

--- src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -3,14 +3,14 @@ package br.com.alura.screenmatch.principal;
 import br.com.alura.screenmatch.model.DadosEpisodios;
 import br.com.alura.screenmatch.model.DadosSerie;
 import br.com.alura.screenmatch.model.DadosTemporada;
+import br.com.alura.screenmatch.model.Episodio;
 import br.com.alura.screenmatch.service.ConsumoAPI;
 import br.com.alura.screenmatch.service.ConverteDados;
 
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Principal {
@@ -20,10 +20,11 @@ public class Principal {
     private ConverteDados conversor = new ConverteDados();
     private final String ENDERECO = "https://www.omdbapi.com/?t=";
     private final String API_KEY = "&apikey=d3f5c35f";
+
     public Principal() {
     }
 
-    public void exibeMenu(){
+    public void exibeMenu() {
         System.out.println("Digite o nome da série para buscar: ");
         var nomeSerie = leitura.nextLine();
         var json = consumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + API_KEY);
@@ -32,22 +33,22 @@ public class Principal {
 
         List<DadosTemporada> temporadas = new ArrayList<>();
 
-			for (int i = 1; i<=dados.totalTemporadas(); i++){
-				json = consumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") +
-						"&season=" + i + API_KEY);
-				DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
-				temporadas.add(dadosTemporada);
-			}
-			temporadas.forEach(System.out::println);
+        for (int i = 1; i <= dados.totalTemporadas(); i++) {
+            json = consumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") +
+                    "&season=" + i + API_KEY);
+            DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
+            temporadas.add(dadosTemporada);
+        }
+        temporadas.forEach(System.out::println);
 
-            for (int i = 0; i < dados.totalTemporadas(); i++){
-                List<DadosEpisodios> episodiosTemporada = temporadas.get(i).episodios();
-                for (int j = 0; j < episodiosTemporada.size(); j++){
-                    System.out.println(episodiosTemporada.get(j).titulo());
-                }
+        for (int i = 0; i < dados.totalTemporadas(); i++) {
+            List<DadosEpisodios> episodiosTemporada = temporadas.get(i).episodios();
+            for (int j = 0; j < episodiosTemporada.size(); j++) {
+                System.out.println(episodiosTemporada.get(j).titulo());
             }
+        }
 
-            temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
+        temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
 
 //            List<String> nomes = Arrays.asList("Jacque", "Iasmin", "Paulo", "Rodrigo", "Nico","Iolanda");
 //            nomes.stream()
@@ -59,6 +60,37 @@ public class Principal {
 
         List<DadosEpisodios> dadosEpisodios = temporadas.stream()
                 .flatMap(t -> t.episodios().stream())
-                .collect(Collectors.toList());
+                .collect(Collectors.toList()); //se a lista precisar ser alterada em algum momento
+        //       .toList(); //to list dá uma lista de dados imutáveis, ou seja, sem permitir alguma edição
+
+        // dadosEpisodios.add(new DadosEpisodios("teste", 3, "10", "2020-01-01")); // permite adicionar mais dados na lista
+
+        System.out.println("\nTop 5 episódios");
+        dadosEpisodios.stream()
+                .filter(e -> !e.avaliacao().equalsIgnoreCase("N/A")) //ignora o que tiver como avaliacao N/A
+                .sorted(Comparator.comparing(DadosEpisodios::avaliacao).reversed()) //ordena a lista pelo numero de avaliações em ordem decrescente
+                .limit(5) // limita a lista em 5
+                .forEach(System.out::println);
+
+        List<Episodio> episodios = temporadas.stream()
+                .flatMap(t -> t.episodios().stream()
+                        .map(d -> new Episodio(t.numeroTemporada(), d))
+                ).collect(Collectors.toList());
+
+        episodios.forEach(System.out::println);
+        System.out.println("A partir de que ano você deseja ver os episódios? ");
+
+        var ano = leitura.nextInt();
+        leitura.nextLine();
+
+        LocalDate dataBusca = LocalDate.of(ano, 1, 1);
+
+        DateTimeFormatter formatador = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        episodios.stream()
+                .filter(e -> e.getDataLancamento() != null && e.getDataLancamento().isAfter(dataBusca))
+                .forEach(e -> System.out.println(
+                        "Temporada: " + e.getTemporada()
+                                + " Episódio: " + e.getTitulo() +
+                                " Data lançamento: " + e.getDataLancamento().format(formatador)));
     }
 }
